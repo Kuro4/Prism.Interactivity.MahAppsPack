c178f769157b2ccd3d757cf4c6910bd47f09cdad
okada <yoyh9ab@outlook.jp>
2018-03-15 17:24:27 +0900

    プロジェクト ファイルを追加します。

---
+++ Prism.InteractivityExtension-Samples/MahApps-Sample/MetroWindow-SampleViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Interactivity.InteractionRequest;
+using Prism.InteractivityExtension;
+using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.InteractivityExtension_Samples.MahApps_Sample
+{
+    public class MetroWindow_SampleViewModel
+    {
+        public ReactiveProperty<Accents> Accent { get; private set; } = new ReactiveProperty<Accents>(Accents.Blue);
+        public ReactiveProperty<Themes> Theme { get; private set; } = new ReactiveProperty<Themes>(Themes.BaseLight);
+
+        public InteractionRequest<Notification> Notification { get; private set; } = new InteractionRequest<Notification>();
+        public InteractionRequest<Confirmation> Confirmation { get; private set; } = new InteractionRequest<Confirmation>();
+
+        public ReactiveCommand RaiseNotification { get; private set; } = new ReactiveCommand();
+        public ReactiveCommand RaiseConfirmation { get; private set; } = new ReactiveCommand();
+
+        public MetroWindow_SampleViewModel()
+        {
+            this.RaiseNotification.Subscribe(() => this.Notification.RaiseEx("Notification", "Notification!", n => Console.WriteLine("Notification")));
+            this.RaiseConfirmation.Subscribe(() => this.Confirmation.RaiseEx("Confirmation", "Confirmation?", n => Console.WriteLine($"Confirmation:{n.Confirmed}")));
+        }
+    }
+}
---
+++ Prism.InteractivityExtension-Samples/MahApps-Sample/PopupMetroWindow-SampleAction.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Interactivity.InteractionRequest;
+using Prism.InteractivityExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Prism.InteractivityExtension_Samples.MahApps_Sample
+{
+    public class PopupMetroWindow_SampleAction : PopupWindowActionBase
+    {
+        protected override Window CreateWindow(INotification notification)
+        {
+            return new MetroWindow_Sample() { Title = notification.Title };
+        }
+
+        protected override void ApplyNotificationToWindow(Window window, INotification notification)
+        {
+        }
+    }
+}

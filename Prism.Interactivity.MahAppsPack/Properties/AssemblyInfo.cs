3ccb557b4500b8fa4b5677fc8197fb508833c7b8
okada <yoyh9ab@outlook.jp>
2019-03-27 08:37:18 +0900

    chore:バージョン情報変更

--- Prism.Interactivity.MahAppsPack/Properties/AssemblyInfo.cs
+++ Prism.Interactivity.MahAppsPack/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("Prism.InteractivityExtension.MahAppsPack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PrismのInteractionRequestでMetroWindowをホストにしたダイアログを表示できるようにするライブラリ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("Prism.InteractivityExtension.MahAppsPack")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.*")]
